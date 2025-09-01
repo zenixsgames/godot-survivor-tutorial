3051b0199be1edb344de18aef22cb029e00227cc
Zenixs Games <zenixsgames@gmail.com>
2025-09-01 20:42:46 +0900

    Fix animation

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -5,5 +5,5 @@ func _ready() -> void:
 	$AnimationPlayer.play("explode")
 
 
-func _on_animation_finished() -> void:
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	queue_free()
